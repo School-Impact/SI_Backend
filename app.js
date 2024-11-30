a30be9feb6a149e67347d2390f124249a549d711
Teguh Iqbal <teguh.iqbal782@gmail.com>
2024-11-30 21:45:21 +0700

    test ci/cd

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use("/user", userRouter);
 
 // Root
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "OK!" });
+  return res.status(200).json({ message: "Test CI / CD!" });
 });
 
 // Check if server is running
