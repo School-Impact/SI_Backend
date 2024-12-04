b6ce5c50ca58eb929c46bc20106fd09f8814adda
Teguh Iqbal <teguh.iqbal782@gmail.com>
2024-12-04 15:00:04 +0700

    add prediction & edit config storage

--- config/index.js
+++ config/index.js
@@ -1,26 +1,20 @@
-/**
- * === Especially for serviceKey variable ===
- * Generate key from service account :
- * Navigation menu => IAM & Admin => Service Accounts => Choose service account => Click 'Keys' tab => Click 'add key' & 'create new key' => Choose 'JSON' as key type
- * The file will be downloaded
- * Copy or Cut key JSON and paste in config folder same as the index config (not mandatory, not have to same as index config)
- *
- * === Warning Message ===
- * (node:16512) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
- * (Use `node --trace-deprecation ...` to show where the warning was created)
- *
- * You can ignore this warning, it's from @google-cloud/storage package
- */
-
 const Cloud = require("@google-cloud/storage");
-const path = require("path");
 require("dotenv").config();
-const serviceKey = path.join(__dirname, "./keys.json");
 
 const { Storage } = Cloud;
-const storage = new Storage({
-  keyFilename: serviceKey,
+
+let storageConfig = {
   projectId: process.env.PROJECT_ID,
-});
+};
+
+// Tambahkan keyFilename jika dijalankan secara lokal
+if (process.env.NODE_ENV !== "production") {
+  const path = require("path");
+  const serviceKey = path.join(__dirname, "./keys.json");
+  storageConfig.keyFilename = serviceKey;
+}
+
+// Inisialisasi storage
+const storage = new Storage(storageConfig);
 
 module.exports = storage;
--- routes/user_route.js
+++ routes/user_route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const UserController = require("../controllers/user_controller"); // call controller
-
+const PredictController = require("../controllers/predict_controller");
 const router = express.Router(); // router
 
 const authenticateToken = require("../middlewares/auth_middleware"); // call middleware
@@ -10,5 +10,7 @@ const multerMid = require("../middlewares/multer_middleware");
 router.get("/home", authenticateToken, UserController.home);
 router.get("/profile", authenticateToken, UserController.user);
 router.patch("/update", multerMid.single("image"), UserController.update);
+
+router.post("/predict", authenticateToken, PredictController.predict);
 
 module.exports = router; // export router
